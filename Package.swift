fcbd03b8580efeffb6cdd7854ce9906ce45148d4
Brian O'Byrne <brian.obyrne@protonmail.com>
2024-12-13 14:43:51 -0800

    add xcode swift package template deafaults

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftMoon",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftMoon",
+            targets: ["SwiftMoon"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftMoon"),
+        .testTarget(
+            name: "SwiftMoonTests",
+            dependencies: ["SwiftMoon"]
+        ),
+    ]
+)
